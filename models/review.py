43984bf0db5ce8f9b1675b6212b324e4c3680989
Petro Malamule <133478698+Shaunpetro@users.noreply.github.com>
2024-04-16 21:35:06 +0200

    models update

--- models/review.py
+++ models/review.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-"""Defines the class Review."""
-from models.base_model import BaseModel
+""" Review module for the HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class Review(BaseModel):
-    """
-    Represents a review.
+class Review(BaseModel, Base):
+    """ Review class to store review information """
+    __tablename__ = 'reviews'
 
-    Public class attributes:
-        place_id: string - empty string: it will be the Place.id
-        user_id: string - empty string: it will be the User.id
-        text: string - empty string
-    """
-    place_id = ""
-    user_id = ""
-    text = ""
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
