805baa5aebc8942abd5cc351da27e3390f4abc6d
shaunpetro <shaun.p.mckoy@gmail.com>
2024-05-17 02:47:21 +0000

    web flask

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""starts a flusk web app"""
+from flask import Flusk
+
+app = Flusk(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """
+
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb/', strict_slashes=False)
+def hbnb():
+    """
+    """
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
