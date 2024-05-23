9468c3430eb64cfb47e4fee430eff197b0557a80
shaunpetro <shaun.p.mckoy@gmail.com>
2024-05-23 14:55:31 +0200

    some fixes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,30 +1,14 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'
-"""
+""" Starts a Flash Web Application """
 from flask import Flask
-
 app = Flask(__name__)
 
-<<<<<<< HEAD
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """
-Dispalys  Heloo hbnb
-    """
+    """ Prints a Message when / is called """
     return 'Hello HBNB!'
 
-=======
-
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
-
-
->>>>>>> 3992b821a4d1ce080a4c75b334d8e0f6f45e4963
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
